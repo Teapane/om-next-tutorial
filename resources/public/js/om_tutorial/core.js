fe723d9d5c38539c579c6c6aa718854b1d094127
Teapane <tyler.stephen.long@gmail.com>
2015-11-24 10:02:23 -0700

    New examples
    - State via `atom`
    - Counter

--- resources/public/js/om_tutorial/core.js
+++ resources/public/js/om_tutorial/core.js
@@ -6,10 +6,11 @@ goog.require('om.next');
 goog.require('om.dom');
 cljs.core.enable_console_print_BANG_.call(null);
 cljs.core.println.call(null,"Yo, yo Teeapane");
+om_tutorial.core.app_state = cljs.core.atom.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"count","count",2139924085),(0)], null));
 /**
  * @constructor
  */
-om_tutorial.core.HelloWorld = (function om_tutorial$core$HelloWorld(){
+om_tutorial.core.Counter = (function om_tutorial$core$Counter(){
 var this__19965__auto__ = this;
 React.Component.apply(this__19965__auto__,arguments);
 
@@ -22,19 +23,19 @@ this__19965__auto__.state = {};
 return this__19965__auto__;
 });
 
-om_tutorial.core.HelloWorld.prototype = goog.object.clone(React.Component.prototype);
+om_tutorial.core.Counter.prototype = goog.object.clone(React.Component.prototype);
 
-var x28602_28610 = om_tutorial.core.HelloWorld.prototype;
-x28602_28610.componentWillUpdate = ((function (x28602_28610){
+var x28803_28813 = om_tutorial.core.Counter.prototype;
+x28803_28813.componentWillUpdate = ((function (x28803_28813){
 return (function (next_props__19906__auto__,next_state__19907__auto__){
 var this__19905__auto__ = this;
 om.next.merge_pending_props_BANG_.call(null,this__19905__auto__);
 
 return om.next.merge_pending_state_BANG_.call(null,this__19905__auto__);
-});})(x28602_28610))
+});})(x28803_28813))
 ;
 
-x28602_28610.shouldComponentUpdate = ((function (x28602_28610){
+x28803_28813.shouldComponentUpdate = ((function (x28803_28813){
 return (function (next_props__19906__auto__,next_state__19907__auto__){
 var this__19905__auto__ = this;
 var or__16771__auto__ = cljs.core.not_EQ_.call(null,om.next.props.call(null,this__19905__auto__),goog.object.get(next_props__19906__auto__,"omcljs$value"));
@@ -48,10 +49,10 @@ return cljs.core.not_EQ_.call(null,goog.object.get(this__19905__auto__.state,"om
 return and__16759__auto__;
 }
 }
-});})(x28602_28610))
+});})(x28803_28813))
 ;
 
-x28602_28610.componentWillUnmount = ((function (x28602_28610){
+x28803_28813.componentWillUnmount = ((function (x28803_28813){
 return (function (){
 var this__19905__auto__ = this;
 var r__19911__auto__ = om.next.get_reconciler.call(null,this__19905__auto__);
@@ -68,24 +69,24 @@ return null;
 } else {
 return om.next.protocols.drop_component_BANG_.call(null,indexer__19910__auto__,this__19905__auto__);
 }
-});})(x28602_28610))
+});})(x28803_28813))
 ;
 
-x28602_28610.componentDidUpdate = ((function (x28602_28610){
+x28803_28813.componentDidUpdate = ((function (x28803_28813){
 return (function (prev_props__19908__auto__,prev_state__19909__auto__){
 var this__19905__auto__ = this;
 return om.next.clear_prev_props_BANG_.call(null,this__19905__auto__);
-});})(x28602_28610))
+});})(x28803_28813))
 ;
 
-x28602_28610.isMounted = ((function (x28602_28610){
+x28803_28813.isMounted = ((function (x28803_28813){
 return (function (){
 var this__19905__auto__ = this;
 return cljs.core.boolean$.call(null,goog.object.getValueByKeys(this__19905__auto__,"_reactInternalInstance","_renderedComponent"));
-});})(x28602_28610))
+});})(x28803_28813))
 ;
 
-x28602_28610.componentWillMount = ((function (x28602_28610){
+x28803_28813.componentWillMount = ((function (x28803_28813){
 return (function (){
 var this__19905__auto__ = this;
 var indexer__19910__auto__ = cljs.core.get_in.call(null,om.next.get_reconciler.call(null,this__19905__auto__),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"config","config",994861415),new cljs.core.Keyword(null,"indexer","indexer",-1774914315)], null));
@@ -94,17 +95,17 @@ return null;
 } else {
 return om.next.protocols.index_component_BANG_.call(null,indexer__19910__auto__,this__19905__auto__);
 }
-});})(x28602_28610))
+});})(x28803_28813))
 ;
 
-x28602_28610.render = ((function (x28602_28610){
+x28803_28813.render = ((function (x28803_28813){
 return (function (){
 var this$ = this;
-var _STAR_reconciler_STAR_28603 = om.next._STAR_reconciler_STAR_;
-var _STAR_depth_STAR_28604 = om.next._STAR_depth_STAR_;
-var _STAR_shared_STAR_28605 = om.next._STAR_shared_STAR_;
-var _STAR_instrument_STAR_28606 = om.next._STAR_instrument_STAR_;
-var _STAR_parent_STAR_28607 = om.next._STAR_parent_STAR_;
+var _STAR_reconciler_STAR_28804 = om.next._STAR_reconciler_STAR_;
+var _STAR_depth_STAR_28805 = om.next._STAR_depth_STAR_;
+var _STAR_shared_STAR_28806 = om.next._STAR_shared_STAR_;
+var _STAR_instrument_STAR_28807 = om.next._STAR_instrument_STAR_;
+var _STAR_parent_STAR_28808 = om.next._STAR_parent_STAR_;
 om.next._STAR_reconciler_STAR_ = om.next.get_reconciler.call(null,this$);
 
 om.next._STAR_depth_STAR_ = (om.next.depth.call(null,this$) + (1));
@@ -115,38 +116,45 @@ om.next._STAR_instrument_STAR_ = om.next.instrument.call(null,this$);
 
 om.next._STAR_parent_STAR_ = this$;
 
-try{return React.DOM.div(null,om.util.force_children.call(null,cljs.core.get.call(null,om.next.props.call(null,this$),new cljs.core.Keyword(null,"title","title",636505583))));
-}finally {om.next._STAR_parent_STAR_ = _STAR_parent_STAR_28607;
+try{var map__28809 = om.next.props.call(null,this$);
+var map__28809__$1 = ((((!((map__28809 == null)))?((((map__28809.cljs$lang$protocol_mask$partition0$ & (64))) || (map__28809.cljs$core$ISeq$))?true:false):false))?cljs.core.apply.call(null,cljs.core.hash_map,map__28809):map__28809);
+var count = cljs.core.get.call(null,map__28809__$1,new cljs.core.Keyword(null,"count","count",2139924085));
+return React.DOM.div(null,om.util.force_children.call(null,React.DOM.span(null,om.util.force_children.call(null,[cljs.core.str("Count: "),cljs.core.str(count)].join('')))),om.util.force_children.call(null,React.DOM.button({"onClick": ((function (map__28809,map__28809__$1,count,_STAR_reconciler_STAR_28804,_STAR_depth_STAR_28805,_STAR_shared_STAR_28806,_STAR_instrument_STAR_28807,_STAR_parent_STAR_28808,this$,x28803_28813){
+return (function (e){
+return cljs.core.swap_BANG_.call(null,om_tutorial.core.app_state,cljs.core.update_in,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"count","count",2139924085)], null),cljs.core.inc);
+});})(map__28809,map__28809__$1,count,_STAR_reconciler_STAR_28804,_STAR_depth_STAR_28805,_STAR_shared_STAR_28806,_STAR_instrument_STAR_28807,_STAR_parent_STAR_28808,this$,x28803_28813))
+},om.util.force_children.call(null,"Click me!"))));
+}finally {om.next._STAR_parent_STAR_ = _STAR_parent_STAR_28808;
 
-om.next._STAR_instrument_STAR_ = _STAR_instrument_STAR_28606;
+om.next._STAR_instrument_STAR_ = _STAR_instrument_STAR_28807;
 
-om.next._STAR_shared_STAR_ = _STAR_shared_STAR_28605;
+om.next._STAR_shared_STAR_ = _STAR_shared_STAR_28806;
 
-om.next._STAR_depth_STAR_ = _STAR_depth_STAR_28604;
+om.next._STAR_depth_STAR_ = _STAR_depth_STAR_28805;
 
-om.next._STAR_reconciler_STAR_ = _STAR_reconciler_STAR_28603;
-}});})(x28602_28610))
+om.next._STAR_reconciler_STAR_ = _STAR_reconciler_STAR_28804;
+}});})(x28803_28813))
 ;
 
 
-om_tutorial.core.HelloWorld.prototype.constructor = om_tutorial.core.HelloWorld;
+om_tutorial.core.Counter.prototype.constructor = om_tutorial.core.Counter;
 
-om_tutorial.core.HelloWorld.prototype.om$isComponent = true;
+om_tutorial.core.Counter.prototype.om$isComponent = true;
 
-var x28608_28611 = om_tutorial.core.HelloWorld;
-
-
-var x28609_28612 = om_tutorial.core.HelloWorld.prototype;
+var x28811_28814 = om_tutorial.core.Counter;
 
 
-om_tutorial.core.HelloWorld.cljs$lang$type = true;
+var x28812_28815 = om_tutorial.core.Counter.prototype;
 
-om_tutorial.core.HelloWorld.cljs$lang$ctorStr = "om-tutorial.core/HelloWorld";
 
-om_tutorial.core.HelloWorld.cljs$lang$ctorPrWriter = (function (this__19967__auto__,writer__19968__auto__,opt__19969__auto__){
-return cljs.core._write.call(null,writer__19968__auto__,"om-tutorial.core/HelloWorld");
+om_tutorial.core.Counter.cljs$lang$type = true;
+
+om_tutorial.core.Counter.cljs$lang$ctorStr = "om-tutorial.core/Counter";
+
+om_tutorial.core.Counter.cljs$lang$ctorPrWriter = (function (this__19967__auto__,writer__19968__auto__,opt__19969__auto__){
+return cljs.core._write.call(null,writer__19968__auto__,"om-tutorial.core/Counter");
 });
-om_tutorial.core.hello = om.next.factory.call(null,om_tutorial.core.HelloWorld);
-ReactDOM.render(om_tutorial.core.hello.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"title","title",636505583),"Hello, world! It's a sunny 10% kind of day!"], null)),goog.dom.getElement("app"));
+om_tutorial.core.reconciler = om.next.reconciler.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"state","state",-1988618099),om_tutorial.core.app_state], null));
+om.next.add_root_BANG_.call(null,om_tutorial.core.reconciler,om_tutorial.core.Counter,goog.dom.getElement("app"));
 
 //# sourceMappingURL=core.js.map
--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -3,17 +3,49 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+;; prints to console
 (enable-console-print!)
-
 (println "Yo, yo Teeapane")
 
-(defui HelloWorld
+;; introduce state into application via global atom
+(def app-state (atom {:count 0}))
+
+;; creates incremental counter button
+(defui Counter
   Object
   (render [this]
-    (dom/div nil (get (om/props this) :title))))
+    (let [{:keys [count]} (om/props this)]
+      (dom/div nil
+        (dom/span nil (str "Count: " count))
+        (dom/button
+          #js {:onClick
+               (fn [e]
+                 (swap! app-state update-in [:count] inc))}
+          "Click me!")))))
 
-(def hello (om/factory HelloWorld))
+;; re-renders root
+(def reconciler
+  (om/reconciler {:state app-state}))
 
-(js/ReactDOM.render
-  (hello {:title "Hello, world! It's a sunny 10% kind of day!"})
-  (gdom/getElement "app"))
+(om/add-root! reconciler
+  Counter (gdom/getElement "app"))
+
+;; first example
+;;(defui HelloWorld
+  ;;Object
+  ;;(render [this]
+    ;;(dom/div nil (get (om/props this) :title))))
+
+;;(def hello (om/factory HelloWorld))
+
+;; first example
+;;(js/ReactDOM.render
+  ;;(hello {:title "Hello, World!"})
+  ;;(gdom/getElement "app"))
+
+;; Second Example
+;;(js/ReactDOM.render
+  ;;(apply dom/div nil
+    ;;(map #(hello {:title (str "Hello " %)})
+      ;;(range 10)))
+  ;;(gdom/getElement "app"))
